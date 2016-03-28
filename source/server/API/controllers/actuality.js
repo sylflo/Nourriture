2d2fa7e2f3bf72a8e85a1719ac169bb3ff78da62
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-03-28 15:10:08 +0800

    find recipe that user we follow and order them by date_posted

--- source/server/API/controllers/actuality.js
+++ source/server/API/controllers/actuality.js
@@ -12,26 +12,26 @@ exports.getActuality = function (req, res) {
 
 
     for (var i = 0; i < req.user.follow.length; i++) {
-        console.log("user that follows me ", req.user.follow[i].id_person);
+        //console.log("user that follows me ", req.user.follow[i].id_person);
 
-        Recipes.find({author_id: req.user.follow[i].id_person}, function(err, recipe) {
-
+        Recipes.find({author_id: req.user.follow[i].id_person}).sort('-date_posted').exec(function(err, recipe){
             if (err) {
                 return (res.status(200).json({message: 'error when getting recipes user following'}));
             }
 
             else {
-                console.log("REcipe = ", recipe);
                 recipe_list.push(recipe);
+                console.log("REcipe list = ", recipe_list);
+                return  res.status(200).json({message: 'Getting actualiity', recipe_list: recipe_list});
             }
-
         });
+
     }
 
 
 
 
-    return  res.status(200).json({message: 'Getting actualiity'});
+
     //return (res.status(200).json({message: 'Getting actualiity'}));
     //Need to find the recpie like by the group the user followed
 
