01c95c870e44ab6e631b2d33715b5560eea75d96
Sylvain Chateau <sylvain.chateau@epitech.eu>
2016-04-10 10:38:13 +0800

    call to front when deleting group

--- source/web/src/app/components/groupPage/groupPage.controller.js
+++ source/web/src/app/components/groupPage/groupPage.controller.js
@@ -1,70 +1,97 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('NourritureControllers')
-	.controller('GroupPageController', GroupPageController);
+  angular.module('NourritureControllers')
+    .controller('GroupPageController', GroupPageController);
 
-GroupPageController.$inject = ["$scope", "$rootScope", "GroupService", "UserService", 'toastr', '$state', "$log", "$stateParams", "$localStorage", "$sessionStorage"];
+  GroupPageController.$inject = ["$scope", "$rootScope", "GroupService", "UserService", 'toastr', '$state', "$log", "$stateParams", "$localStorage", "$sessionStorage"];
 
-function GroupPageController($scope, $rootScope, GroupService, UserService, toastr, $state, $log, $stateParams, $localStorage, $sessionStorage)
-{
-	var vm = this;
+  function GroupPageController($scope, $rootScope, GroupService, UserService, toastr, $state, $log, $stateParams, $localStorage, $sessionStorage) {
+    var vm = this;
 
-	vm.GroupFetchSuccess = function (data) {
-		$log.log(data);
-		vm.group = data;
-		vm.userIsIn = 0;
-		vm.userIsOwner = 0;
-		for (var i = 0; i != data.users.length; i++) {
-			if (data.users[i].user_id === ($localStorage.user_id || $sessionStorage.user_id)) {
-				vm.userIsIn = 1;
-				if (data.users[i].access.name === "Admin")
-					vm.userIsOwner = 1;
-			}
-		}
-	};
+    vm.GroupFetchSuccess = function (data) {
+      $log.log(data);
+      vm.group = data;
+      vm.userIsIn = 0;
+      vm.userIsOwner = 0;
+      for (var i = 0; i != data.users.length; i++) {
+        if (data.users[i].user_id === ($localStorage.user_id || $sessionStorage.user_id)) {
+          vm.userIsIn = 1;
+          if (data.users[i].access.name === "Admin")
+            vm.userIsOwner = 1;
+        }
+      }
+    };
 
-	vm.GroupFetchFailure = function (data) {
-		$log.log(data.data);
-		var errorMsg = "This is odd...";
-		toastr.error(errorMsg, 'Woops...');
-	};
+    vm.GroupFetchFailure = function (data) {
+      $log.log(data.data);
+      var errorMsg = "This is odd...";
+      toastr.error(errorMsg, 'Woops...');
+    };
 
-	vm.registerUser = function() {
-		GroupService.add_user
-		.update({"group_id": vm.group._id, "user_id": $localStorage.user_id})
-		.$promise
-		.then(vm.getGroup());
+    vm.SuccessDeleteGroup = function () {
+      toastr.success('Group successfully deleted');
+    };
 
-		UserService.addGroupToUser
-		.update({id: $localStorage.user_id || $sessionStorage.user_id, "group_id": vm.group._id, "group_name": vm.group.name})
-		.$promise;
-		vm.userIsIn = 1;
-	}
+    vm.ErrorDeleteGroup = function () {
+      $log.log(data.data);
+      var errorMsg = "Error deleting group";
+      toastr.error(errorMsg, 'Woops...');
+    };
 
-	vm.unregisterUser = function() {
-		GroupService.remove_user
-		.update({"group_id": vm.group._id, "user_id": $localStorage.user_id})
-		.$promise
-		.then(vm.getGroup());
+    vm.deleteGroup = function () {
 
-		UserService.removeGroupToUser
-		.update({id: $localStorage.user_id || $sessionStorage.user_id, "group_id": vm.group._id, "group_name": vm.group.name})
-		.$promise;
+      GroupService
+        .delete_by_id
+        .delete({"id": vm.group._id})
+        .$promise
+        .then(vm.SuccessDeleteGroup, vm.ErrorDeleteGroup);
 
-		vm.userIsIn = 0;
-		vm.userIsOwner = 0;
-	}
+    };
 
-	vm.getGroup = function() {
-		GroupService
-			.group_id
-			.get({"id" : $stateParams.id})
-			.$promise
-			.then(vm.GroupFetchSuccess, vm.GroupFetchFailure);
-	}
+    vm.registerUser = function () {
+      GroupService.add_user
+        .update({"group_id": vm.group._id, "user_id": $localStorage.user_id})
+        .$promise
+        .then(vm.getGroup());
 
-	vm.getGroup();
-}
+      UserService.addGroupToUser
+        .update({
+          id: $localStorage.user_id || $sessionStorage.user_id,
+          "group_id": vm.group._id,
+          "group_name": vm.group.name
+        })
+        .$promise;
+      vm.userIsIn = 1;
+    }
+
+    vm.unregisterUser = function () {
+      GroupService.remove_user
+        .update({"group_id": vm.group._id, "user_id": $localStorage.user_id})
+        .$promise
+        .then(vm.getGroup());
+
+      UserService.removeGroupToUser
+        .update({
+          id: $localStorage.user_id || $sessionStorage.user_id,
+          "group_id": vm.group._id,
+          "group_name": vm.group.name
+        })
+        .$promise;
+
+      vm.userIsIn = 0;
+      vm.userIsOwner = 0;
+    }
+
+    vm.getGroup = function () {
+      GroupService
+        .group_id
+        .get({"id": $stateParams.id})
+        .$promise
+        .then(vm.GroupFetchSuccess, vm.GroupFetchFailure);
+    }
+
+    vm.getGroup();
+  }
 
 })();
--- source/web/src/app/components/groups/Groups.services.js
+++ source/web/src/app/components/groups/Groups.services.js
@@ -1,25 +1,34 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('NourritureServices')
-        .factory('GroupService', GroupService);
+  angular
+    .module('NourritureServices')
+    .factory('GroupService', GroupService);
 
-    GroupService.$inject = ["$log", "$resource", "URL_API"];
+  GroupService.$inject = ["$log", "$resource", "URL_API"];
 
-    /** @ngInject */
-    function GroupService($log, $resource, URL_API) {
+  /** @ngInject */
+  function GroupService($log, $resource, URL_API) {
 
-        var service = {
-            groups: $resource(URL_API + '/api/groups/'),
-            group_name : $resource(URL_API + '/api/groups/name/:name', {name: "@name"}),
-            group_id : $resource(URL_API + '/api/groups/id/:id', {id: "@id"}),
-            add_user : $resource(URL_API + '/api/groups/:group_id/add/:user_id', {group_id: "@group_id", user_id: "@user_id"}, {'update': {method: 'PUT'}}),
-            remove_user : $resource(URL_API + '/api/groups/:group_id/remove/:user_id', {group_id: "@group_id", user_id: "@user_id"}, {'update': {method: 'PUT'}})
+    var service = {
+      groups: $resource(URL_API + '/api/groups/'),
+      group_name: $resource(URL_API + '/api/groups/name/:name', {name: "@name"}),
+      group_id: $resource(URL_API + '/api/groups/id/:id', {id: "@id"}),
+      add_user: $resource(URL_API + '/api/groups/:group_id/add/:user_id', {
+        group_id: "@group_id",
+        user_id: "@user_id"
+      }, {'update': {method: 'PUT'}}),
+      remove_user: $resource(URL_API + '/api/groups/:group_id/remove/:user_id', {
+        group_id: "@group_id",
+        user_id: "@user_id"
+      }, {'update': {method: 'PUT'}}),
+      delete_by_id: $resource(URL_API + '/api/groups/id/:id', {id: "@id"}),
+      delete_by_name: $resource(URL_API + '/api/groups/name/:name', {name: "@name"})
 
-        };
 
-        return service;
+    };
 
-    }
+    return service;
+
+  }
 })();
